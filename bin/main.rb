f7329b0833823ef0abb314f14b3689c165650cf8
Adam Ulrich <a_ulrich@utb.cz>
2023-05-17 16:38:52 +0200

    fix main in module

--- bin/main.rb
+++ bin/main.rb
@@ -3,14 +3,17 @@
 require "bundler/setup"
 require "adamforest"
 
-include AdamForest
+module Main
+  include AdamForest
 
-puts "main"
+  puts "main"
 
-forest = Forest.new([[1, 1], [2, 2], [3, 3], [7, 1000]], trees_count: 1000)
-p [[2.5, 1.5], [2, 100], [7, 50]].map { |x|
-                                          r = forest.evaluate_forest(x)
-                                          r.sum / r.count.to_f
-                                        }
-evaluation = forest.evaluate_forest([2.5, 1.5])
-#p evaluation
+  forest = Forest.new([[1, 1], [2, 2], [3, 3], [7, 1000]], trees_count: 1000)
+  p([[2.5, 1.5], [2, 100], [7, 50]].map do |x|
+    r = forest.evaluate_forest(x)
+    r.sum / r.count.to_f
+  end)
+
+  evaluation = forest.evaluate_forest([2.5, 1.5])
+  p evaluation
+end
